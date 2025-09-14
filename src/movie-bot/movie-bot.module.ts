f4f54a9fbab574124a1b549ae0204d89e6be127c
sivakumarrc32 <sivakumarrconline@gmail.com>
2025-09-14 17:09:42 +0530

    Last Updated

--- src/movie-bot/movie-bot.module.ts
+++ src/movie-bot/movie-bot.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MovieBotService } from './movie-bot.service';
 import { Movie, MovieSchema } from './movie.schema';
+import { MovieBotController } from './movie-bot.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Movie.name, schema: MovieSchema }]),
   ],
   providers: [MovieBotService],
+  controllers: [MovieBotController],
 })
 export class MovieBotModule {}
--- src/upload-bot/upload-bot.module.ts
+++ src/upload-bot/upload-bot.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UploadBotService } from './upload-bot.service';
 import { Movie, MovieSchema } from '../movie-bot/movie.schema';
+import { UploadBotController } from './upload-bot.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Movie.name, schema: MovieSchema }]),
   ],
   providers: [UploadBotService],
+  controllers: [UploadBotController],
 })
 export class UploadBotModule {}
