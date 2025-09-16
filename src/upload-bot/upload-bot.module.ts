209cd7fe6e111fd11ede9a6bc23e75f731939d64
sivakumarrc32 <sivakumarrconline@gmail.com>
2025-09-16 22:08:16 +0530

    bug cleared

--- src/upload-bot/upload-bot.module.ts
+++ src/upload-bot/upload-bot.module.ts
@@ -4,10 +4,14 @@ import { UploadBotService } from './upload-bot.service';
 import { Movie, MovieSchema } from '../movie-bot/movie.schema';
 import { UploadBotController } from './upload-bot.controller';
 import { MovieBotService } from 'src/movie-bot/movie-bot.service';
+import { User, UserSchema } from 'src/movie-bot/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Movie.name, schema: MovieSchema }]),
+    MongooseModule.forFeature([
+      { name: Movie.name, schema: MovieSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   providers: [UploadBotService, MovieBotService],
   controllers: [UploadBotController],
