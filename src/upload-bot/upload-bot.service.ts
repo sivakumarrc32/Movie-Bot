7d90e391f402a25b68e9676636e14f37da99f427
sivakumarrc32 <sivakumarrconline@gmail.com>
2025-09-27 12:53:21 +0530

    Update

--- src/upload-bot/upload-bot.service.ts
+++ src/upload-bot/upload-bot.service.ts
@@ -32,6 +32,7 @@ export class UploadBotService implements OnModuleInit {
 
   private checkOwner(ctx: any): boolean {
     if (ctx.from.id !== (this.ownerId || this.ownerID2)) {
+      console.log(this.ownerID2);
       ctx.reply(
         '<b>🚫 You are not authorized to use this bot.</b> \n\n\n @lord_fourth_movie_bot Here You Can Get the Movies',
         {
