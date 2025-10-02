c30589bab10b980fde82f13c40998d30d05837a9
sivakumarrc32 <sivakumarrconline@gmail.com>
2025-10-03 01:23:37 +0530

    Update

--- src/anime/anime.service.ts
+++ src/anime/anime.service.ts
@@ -55,7 +55,7 @@ export class AnimeService implements OnModuleInit {
     this.bot.action('help', (ctx) => this.help(ctx));
     this.bot.action('about', (ctx) => this.about(ctx));
     this.bot.action('backToStart', (ctx) => this.backToStart(ctx));
-    this.bot.on('message', (ctx) => console.log(ctx.message));
+    // this.bot.on('message', (ctx) => console.log(ctx.message));
   }
 
   expireAt = new Date(Date.now() + 5 * 60 * 1000);
